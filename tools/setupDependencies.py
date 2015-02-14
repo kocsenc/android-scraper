3809ed79071c8f1e5ac4825635afd192899e711f
kocsen <kocsenc@gmail.com>
2015-02-14 11:27:52 -0500

    updating cleanup script for dependency setup

--- tools/setupDependencies.py
+++ tools/setupDependencies.py
@@ -23,19 +23,28 @@ def main():
     extract(dex2jar_dir)
 
 
-def extract(path):
-    print("Extracting " + path)
-    with zipfile.ZipFile(path, "r") as z:
+def extract(path_to_zip):
+    print("Extracting " + path_to_zip)
+    with zipfile.ZipFile(path_to_zip, "r") as z:
         z.extractall(lib_dir)
+
+    zip_name = os.path.basename(dex2jar_url).split(".zip")[0]
+    # Rename dex2jar.#.#.# to dex2jar
+    os.rename(lib_dir + "/" + zip_name, lib_dir + "/dex2jar")
+    # Remove the zip
+    os.remove(dex2jar_dir)
 
 
 def create_lib_dir():
     """
-    Creates a /lib directory in the working directory if it doesent already exist.
+    Creates a /lib directory in the working directory if it doesent already exist,
+    if dir exists, deletes it and makes a new one.
     """
-    if not os.path.exists(lib_dir):
-        print("Creating lib directory")
-        os.makedirs(lib_dir)
+    if os.path.exists(lib_dir):
+        print("Existing lib directory is being deleted...")
+        os.rmdir(lib_dir)
+    print("Creating new lib directory")
+    os.makedirs(lib_dir)
 
 
 if __name__ == "__main__":
