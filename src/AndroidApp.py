456e63e05489f95eddbb41414d2dc85d69a025bb
Kocsen <kocsenc@gmail.com>
2015-03-10 14:16:27 -0400

    - xml traversal support

--- src/AndroidApp.py
+++ src/AndroidApp.py
@@ -1,3 +1,4 @@
+import fnmatch
 import os
 
 __author__ = 'kocsen'
@@ -15,6 +16,8 @@ class AndroidApp():
     def __init__(self, name, location_root):
         self.name = name
         self.location = location_root
+
+        # getting the source JAVA files
         self.code_source_location = os.path.join(location_root, "app/src")
         self.manifest_ET_root = ET.parse(location_root + '/app/AndroidManifest.xml').getroot()
 
@@ -22,6 +25,14 @@ class AndroidApp():
         for root, subFolders, files in os.walk(self.code_source_location):
             for filename in files:
                 self.source_paths.append(os.path.join(root, filename))
+
+        # getting the source RES files
+        self.res_source_location = os.path.join(location_root, "app/res")
+        self.res_paths = []
+        for root, subFolders, files in os.walk(self.res_source_location):
+            for filename in fnmatch.filter(files, '*.xml'):  # Matching XML
+                self.res_paths.append(os.path.join(root, filename))
+
 
 
 
