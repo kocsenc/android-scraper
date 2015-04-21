ab0401d82678577cf5c454c826f0bde94c993980
Kocsen <kocsenc@gmail.com>
2015-04-21 14:51:07 -0400

    another instance to push

--- src/BatchRun.py
+++ src/BatchRun.py
@@ -12,10 +12,10 @@ from Driver import analyze_app
 
 
 def main():
-    batch(sys.argv[1], sys.argv[2])
+    batch(sys.argv[1], sys.argv[2], sys.argv[3])
 
 
-def batch(app_directory, decompiler_script):
+def batch(app_directory, file_with_apknames, decompiler_script):
     """
 
     :param app_directory:
@@ -24,11 +24,12 @@ def batch(app_directory, decompiler_script):
     """
     app_directory = os.path.abspath(app_directory)
     decompiler_script = os.path.abspath(decompiler_script)
-    logging.info("APPS:    " + app_directory)
-    logging.info("DECOMPLR:" + decompiler_script)
+    print("APPS:    " + app_directory)
+    print("DECOMPLR:" + decompiler_script)
 
-    for original_apk_file in os.listdir(app_directory):
-        logging.info("Decompiling and assessing " + os.path.basename(original_apk_file))
+    # for original_apk_file in os.listdir(app_directory):
+    for original_apk_file in get_apk_paths_given_filename(app_directory, file_with_apknames):
+        print("Decompiling and assessing " + os.path.basename(original_apk_file))
         if False:  # TEMPORARY TODO: remove
             try:
                 apk_absolute_path = os.path.abspath(original_apk_file)
@@ -46,6 +47,18 @@ def batch(app_directory, decompiler_script):
                 if os.path.isdir(abs_uncompressed_path) and os.path.exists(abs_uncompressed_path):
                     logging.info("Deleting uncompressed directory")
                     shutil.rmtree(abs_uncompressed_path)
+
+
+def get_apk_paths_given_filename(apps_path, filename):
+    """
+    :param apps_path: The path to where all the apks are located
+    :param filename:  The filename that has the list of all the app names
+    :return: a list of the absolute paths for the actual locations of the apk's
+    """
+    with open(filename, 'w') as f:
+        file_lines = f.readlines()
+
+    return [os.path.join(apps_path, app_name) for app_name in file_lines]
 
 
 if __name__ == "__main__":
