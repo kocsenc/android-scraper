12db9e14cabfd04e14dd406aa01f4c0ce149f6b2
Kocsen <kocsenc@gmail.com>
2015-04-23 11:15:46 -0400

    setup logging

--- src/BatchRun.py
+++ src/BatchRun.py
@@ -12,13 +12,15 @@ from Driver import analyze_app
 
 
 def main():
+    logging.basicConfig(filename='out.log', level=logging.DEBUG)
     # USAGE:
     # python BatchRun.py /path/to/apps/ path/to/filename_w_appnames.txt path/to/decompiler.sh
     if len(sys.argv) == 4:
         # TODO: Add argument parsing
         batch(sys.argv[1], sys.argv[2], sys.argv[3])
     else:
-        print("USAGE:\n  python BatchRun.py /path/to/apps/ path/to/filename_w_appnames.txt path/to/decompiler.sh")
+        logging.info(
+            "USAGE:\n  python BatchRun.py /path/to/apps/ path/to/filename_w_appnames.txt path/to/decompiler.sh")
 
 
 def batch(app_directory, file_with_apknames, decompiler_script):
@@ -32,25 +34,24 @@ def batch(app_directory, file_with_apknames, decompiler_script):
     decompiler_script = os.path.abspath(decompiler_script)
     file_with_apknames = os.path.abspath(file_with_apknames)
 
-    print("APPS:    " + app_directory)
-    print("DECOMPLR:" + decompiler_script)
-    print("APPNAMES:" + file_with_apknames)
+    logging.info("APPS:    " + app_directory)
+    logging.info("DECOMPLR:" + decompiler_script)
+    logging.info("APPNAMES:" + file_with_apknames)
 
     # for original_apk_file in os.listdir(app_directory):
     for original_apk_file in get_apk_paths_given_filename(app_directory, file_with_apknames):
-        print("Decompiling and assessing " + os.path.basename(original_apk_file))
+        logging.info("Decompiling and assessing " + os.path.basename(original_apk_file))
         apk_absolute_path = os.path.abspath(original_apk_file)
         abs_uncompressed_path = ""
 
         try:
             # Step 1 : decompile
-            command = decompiler_script + " " + apk_absolute_path
             subprocess.call([decompiler_script, apk_absolute_path])  # apkdecompiler.sh /apks/app.apk
 
             # Step 2 : get uncompressed directory created above and call analysis
             abs_uncompressed_path = os.path.join(os.path.dirname(decompiler_script),
                                                  os.path.basename(apk_absolute_path) + ".uncompressed")
-            print("Uncompressed Path: " + apk_absolute_path)
+            logging.info("Uncompressed Path: " + apk_absolute_path)
             analyze_app(abs_uncompressed_path)
         finally:
             # Hopefully the uncompressed app has been analyzed, now remove it
