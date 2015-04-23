34e1b91fd45b95f984373c89e1ab8acfd92b30d0
Kocsen <kocsenc@gmail.com>
2015-04-23 11:38:23 -0400

    moving when absolute paths are calculated

--- src/BatchRun.py
+++ src/BatchRun.py
@@ -42,25 +42,24 @@ def batch(app_directory, file_with_apknames, decompiler_script):
     for original_apk_file in get_apk_paths_given_filename(app_directory, file_with_apknames):
         logging.info("Decompiling and assessing " + os.path.basename(original_apk_file))
         apk_absolute_path = os.path.abspath(original_apk_file)
-        abs_uncompressed_path = ""
+        apk_name = os.path.basename(apk_absolute_path)
+        uncompressed_apk_name = apk_name + ".uncompressed"
+        uncompressed_apk_absolute_path = os.path.join(os.path.dirname(decompiler_script),
+                                                      uncompressed_apk_name)
 
         try:
             # Step 1 : decompile
             subprocess.call([decompiler_script, apk_absolute_path, subprocess.PIPE,
                              subprocess.DEVNULL])  # apkdecompiler.sh /apks/app.apk
 
-            # Step 2 : get uncompressed directory created above and call analysis
-            uncompressed_dir = os.path.basename(apk_absolute_path) + ".uncompressed"
-            abs_uncompressed_path = os.path.join(os.path.dirname(decompiler_script),
-                                                 uncompressed_dir)
-
-            logging.info("Uncompressed Path: " + abs_uncompressed_path)
-            analyze_app(abs_uncompressed_path)
+            # Step 2 : call analysis on uncompressed apk
+            logging.info("Uncompressed Path: " + uncompressed_apk_absolute_path)
+            analyze_app(uncompressed_apk_absolute_path)
         finally:
             # Hopefully the uncompressed app has been analyzed, now remove it
-            if os.path.isdir(abs_uncompressed_path) and os.path.exists(abs_uncompressed_path):
+            if os.path.isdir(uncompressed_apk_absolute_path) and os.path.exists(uncompressed_apk_absolute_path):
                 logging.info("Deleting uncompressed directory")
-                shutil.rmtree(abs_uncompressed_path)
+                shutil.rmtree(uncompressed_apk_absolute_path)
 
 
 def get_apk_paths_given_filename(apps_path, filename):
